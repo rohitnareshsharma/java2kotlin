c9eec11c14e3a50571dc68a0dd00f80aade32f7a
Rohit Sharma <rohit7.sharma@paytmmall.com>
2020-09-13 15:37:24 +0530

    Added the first lesson of Kotlin Hello world with some introduction

---
+++ src/Lesson1_HelloWorld.kt
@@ -0,0 +1,25 @@
+/**
+ * These lessons are created following kotlin tutorials
+ * over TutorialsPoint https://www.tutorialspoint.com/kotlin/index.htm
+ *
+ * Kotlin = Java + Some Advanced Feature
+ *        = OOPS + Functional Programming
+ *        = More expressive than java. So easy readability
+ *        = Depends internally on JVM Itself
+ *        = Strongly statically typed language
+ *        = programming language developed by Jet brains
+ *
+ * Lets get started with our first hello world program.
+ * Like java no need to define a class and a long public static void main method.
+ * Just a method by the name main will do the trick.
+ *
+ *
+ * Learn about basic function syntax in kotlin.
+ *
+ * Java Syntax = modifier returnType functionName(Arguments) { Body; }
+ * Kotlin Syntax = fun functionName(Arguments) : returnType { Body }
+ *
+ */
+fun main() {
+    System.out.println("Hello World")
+}
